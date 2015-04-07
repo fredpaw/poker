7d535997919f3891b011acf923799364997143a8
fredpaw <fredpaw@gmail.com>
2015-04-07 23:32:05 +1000

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<doctype html>
+<html>
+<head>
+<title>Poker Calc</title>
+</head>
+<body>
+<h3>Choose Game</h3>
+<ul>
+<li><a href="frg.php">Full Ring Game</a></li>
+<li>Short Hand Ring Game</li>
+<li></li>
+</ul>
+</body>
+</html>
